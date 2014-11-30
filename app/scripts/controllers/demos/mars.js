4a05454f8a9cc7c3aa3e1c8c8eee273e8ce6079b
rhodges <rhodges@ecotrust.org>
2014-11-30 08:55:44 -0800

    Cleaning up the map view

--- app/scripts/controllers/demos/mars.js
+++ app/scripts/controllers/demos/mars.js
@@ -11,29 +11,22 @@ if (false) {var OpenLayers = null;}
  */
 angular.module('hodgimotoApp')
   .controller('DemosMarsCtrl', function ($scope) {
-    // $scope.map;
 
-	// function init() {
     $scope.map = new OpenLayers.Map({
       div: 'map',
       allOverlays: false
     });
 
-    // var osm = new OpenLayers.Layer.OSM();
     var mars = new OpenLayers.Layer.OSM(
         'Mars',
         '/images/mars_tiles/Mapnik/${z}/${x}/${y}.png',
-        {numZoomLevels: 9, isBaseLayer: true}
-    );
-    var gmap = new OpenLayers.Layer.Google(
-      'Google Streets',
-      {visibility: false, isBaseLayer: false}
+        {numZoomLevels: 9, isBaseLayer: true, attribution:''}
     );
 
     // note that first layer must be visible
-    $scope.map.addLayers([mars, gmap]);
+    $scope.map.addLayers([mars]);
 
-    $scope.map.addControl(new OpenLayers.Control.LayerSwitcher());
+    // $scope.map.addControl(new OpenLayers.Control.LayerSwitcher());
     $scope.map.zoomToMaxExtent();
 
 	// }
